cf5098f5e69de287d2dae527043e817fb681dcbd
jacob <jmachadoat4@gmail.com>
2021-05-11 00:18:17 -0700

    commiting prior labs

---
+++ src/main/java/edu/ucr/cs/cs167/jmach017/Filter.java
@@ -0,0 +1,34 @@
+package edu.ucr.cs.cs167.jmach017;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+public class Filter {
+    public static void main( String[] args ) {
+        final String inputfile = args[0];
+        final String outputFile = args[1];
+        final String desiredCode = args[2];
+
+
+
+        SparkConf conf = new SparkConf();
+        if (!conf.contains("spark.master")) {
+            conf.setMaster("local[*]");
+        }
+        System.out.printf("Using Spark master '%s'\n", conf.get("spark.master"));
+        conf.setAppName("lab4");
+        JavaSparkContext spark = new JavaSparkContext(conf);
+
+        try {
+
+            JavaRDD<String> logFile = spark.textFile(inputfile);
+            logFile.cache();
+            JavaRDD<String> matchingLines = logFile.filter(line -> line.split("\t")[5].equals(desiredCode));
+            System.out.printf("The file '%s' contains %d lines with response code %s\n", inputfile, matchingLines.count(), desiredCode);
+            matchingLines.saveAsTextFile(outputFile);
+            logFile.unpersist();
+        } finally {
+            spark.close();
+        }
+    }
+}
